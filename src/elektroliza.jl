75efde5154beac878428ad255d65acdb0cb791ac
Carbon225 <carbon225@protonmail.com>
2021-11-10 16:59:34 +0100

    elektroliza

--- src/elektroliza.jl
+++ src/elektroliza.jl
@@ -1,7 +1,30 @@
 module elektroliza
 
+using Unitful
+using Measurements
+import PhysicalConstants.CODATA2018: N_A
+
 function main()
-    
+    μ = 63.58u"g"
+    w = 2
+    t = 30u"minute" ± 5u"s"
+    I = 0.5u"A" ± 3u"mA"
+    m₁ = 110.246u"g" ± 0.1u"g"
+    m₂ = 110.532u"g" ± 0.1u"g"
+
+    m = m₂ - m₁
+    @show m
+
+    k = uconvert(u"mg/C", m/I/t)
+    @show k
+
+    F = uconvert(u"C", (μ/w/k))
+    @show F
+
+    e = ustrip(u"C*mol", F/N_A)u"C"
+    @show e
+
+    nothing
 end
 
 end
